f793af4bb9af113247befc4ff6660dea69b2d75f
Max Bondarenco <zeetreex@gmail.com>
2023-12-08 14:47:49 +0200

    values

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("string" + "string")
+
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("8.13 / 2 = ", 8.13/2)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
